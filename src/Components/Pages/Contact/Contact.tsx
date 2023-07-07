679ef18bd64e05d35c5d5d2e356b94b953977c94
chadrakh <chadrak@outlook.com>
2023-07-07 15:48:45 +0100

    Add react routing

---
+++ src/Components/Pages/Contact/Contact.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+type ContactProps = {
+};
+
+const Contact: FC<ContactProps> = () => {
+  return (
+    <div>
+      This is the 'Contact' page
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/Components/Pages/Projects/Projects.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+type ProjectsProps = {
+};
+
+const Projects: FC<ProjectsProps> = () => {
+  return (
+    <div>
+      This is the 'Projects' page
+    </div>
+  );
+};
+
+export default Projects;
