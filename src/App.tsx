e8e6790654f49af6f172dbb9e514e6ae0756e850
chadrakh <chadrak@outlook.com>
2023-10-20 14:35:17 +0100

    Feat: Add Landing Component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-const App: React.FC = () => {
+import { useEffect } from 'react';
+import Landing from './components/pages/landing';
+
+const App : React.FC = () => {
+  useEffect(() => {
+    document.title = "Chadrak Holondo - Developer Portfolio";
+  }, []);
 
   return (
     <>
+      <Landing />
     </>
-  )
+  );
 }
 
 export default App;
