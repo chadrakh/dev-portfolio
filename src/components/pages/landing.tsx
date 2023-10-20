1007f3c47c93cb57ffde5bd356accb06c2af01ad
chadrakh <chadrak@outlook.com>
2023-10-20 14:34:56 +0100

    Feat: Create Landing Component File

---
+++ src/components/pages/landing.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+const Landing : React.FC = () => {
+    useEffect(() => {
+        document.title = 'Home';
+      }, []);
+      
+    return (
+        <>
+        
+        </>
+    );
+}
+
+export default Landing;
